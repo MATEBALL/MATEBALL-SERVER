3130444774cde46dfd3a5b7a99ae5a8730674972
dajinYang <didekwls0104@naver.com>
2025-06-17 19:36:22 +0900

    [init] initial commit

---
+++ src/main/java/at/mateball/MateballApplication.java
@@ -0,0 +1,13 @@
+package at.mateball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/mateball/MateballApplicationTests.java
@@ -0,0 +1,13 @@
+package at.mateball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
