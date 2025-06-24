f2ff49038d322f6becb509b7965f824fa2ac064a
hyeumm <20221159@sungshin.ac.kr>
2025-06-24 19:12:30 +0900

    [feat/#1] Sample Controller 및 Dto 생성

---
+++ src/main/java/at/mateball/domain/sample/api/controller/SampleController.java
@@ -0,0 +1,21 @@
+package at.mateball.domain.sample.api.controller;
+
+import at.mateball.common.MateballResponse;
+import at.mateball.exception.ErrorCode;
+import at.mateball.domain.sample.api.dto.SampleResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    public SampleController() {
+    }
+
+    @GetMapping("/sample")
+    public MateballResponse<SampleResponseDto> getSampleData() {
+        SampleResponseDto sampleData = new SampleResponseDto("다진언니 안녕");
+
+        return MateballResponse.success(ErrorCode.Success_Sample, sampleData);
+    }
+}
---
+++ src/main/java/at/mateball/domain/sample/api/dto/SampleResponseDto.java
@@ -0,0 +1,6 @@
+package at.mateball.domain.sample.api.dto;
+
+public record SampleResponseDto(
+        String toDajin
+) {
+}
