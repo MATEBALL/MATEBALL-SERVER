4533426e58101d251ac2d5475e14ad658661697a
hyeumm <20221159@sungshin.ac.kr>
2025-06-24 19:11:29 +0900

    [feat/#1] MateballResponse 생성

---
+++ src/main/java/at/mateball/common/MateballResponse.java
@@ -0,0 +1,23 @@
+package at.mateball.common;
+
+import at.mateball.exception.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public record MateballResponse<T>(
+        int status,
+        String message,
+        @JsonInclude(value = JsonInclude.Include.NON_NULL)
+        T data
+) {
+    public static <T> MateballResponse<T> success(ErrorCode successCode, T data) {
+        return new MateballResponse<>(successCode.getStatus().value(), successCode.getMessage(), data);
+    }
+
+    public static <T> MateballResponse<T> successWithNoData(ErrorCode successCode) {
+        return new MateballResponse<>(successCode.getStatus().value(), successCode.getMessage(), null);
+    }
+
+    public static <T> MateballResponse<T> failure(ErrorCode failureCode) {
+        return new MateballResponse<>(failureCode.getStatus().value(), failureCode.getMessage(), null);
+    }
+}
