ca3a68558dd93b41bfe2534587681f6d7d31cfaf
hyeumm <20221159@sungshin.ac.kr>
2025-06-25 21:49:12 +0900

    [chore/#1] dto 파일 네이밍 변경 (dto삭제) 및 response 패키지 적용

--- src/main/java/at/mateball/domain/sample/api/controller/SampleController.java
+++ src/main/java/at/mateball/domain/sample/api/controller/SampleController.java
@@ -2,7 +2,7 @@ package at.mateball.domain.sample.api.controller;
 
 import at.mateball.common.MateballResponse;
 import at.mateball.exception.ErrorCode;
-import at.mateball.domain.sample.api.dto.SampleResponseDto;
+import at.mateball.domain.sample.api.dto.response.SampleResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +13,8 @@ public class SampleController {
     }
 
     @GetMapping("/sample")
-    public MateballResponse<SampleResponseDto> getSampleData() {
-        SampleResponseDto sampleData = new SampleResponseDto("다진언니 안녕");
+    public MateballResponse<SampleResponse> getSampleData() {
+        SampleResponse sampleData = new SampleResponse("다진언니 안녕");
 
         return MateballResponse.success(ErrorCode.SUCCESS_SAMPLE, sampleData);
     }
---
+++ src/main/java/at/mateball/domain/sample/api/dto/response/SampleResponse.java
@@ -0,0 +1,6 @@
+package at.mateball.domain.sample.api.dto.response;
+
+public record SampleResponse(
+        String toDajin
+) {
+}
