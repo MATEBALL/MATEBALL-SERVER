a6302dbd4a8316a56daf8f9ce007c88d4beb87df
dajinYang <didekwls0104@naver.com>
2025-06-29 02:59:07 +0900

    [feat/#3] CustomAuthenticationEntryPoint 인증 에러 클래스 추가

---
+++ src/main/java/at/mateball/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package at.mateball.exception;
+
+import at.mateball.common.MateballResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authenticationException
+    ) throws IOException {
+        MateballResponse<?> errorResponse = MateballResponse.failure(ErrorCode.UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
