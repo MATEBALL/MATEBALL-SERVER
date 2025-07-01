a9444be60bbaca93bced64653d1b4d30a272461a
dajinYang <didekwls0104@naver.com>
2025-07-02 04:49:26 +0900

    [feat/#13] 카카오 사용자 아이디 조회

--- src/main/java/at/mateball/domain/user/UserRepository.java
+++ src/main/java/at/mateball/domain/user/UserRepository.java
@@ -2,5 +2,8 @@ package at.mateball.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByKakaoUserId(Long kakaoUserId);
 }
