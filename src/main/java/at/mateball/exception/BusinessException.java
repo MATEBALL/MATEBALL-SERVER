b992b77eeb3105aa1fc4968e6df8c60e26abaa42
dajinYang <didekwls0104@naver.com>
2025-07-01 23:58:16 +0900

    [feat/#11] business 에러 처리

--- src/main/java/at/mateball/exception/BusinessException.java
+++ src/main/java/at/mateball/exception/BusinessException.java
@@ -1,3 +1,14 @@
 package at.mateball.exception;
-public class BusinessException {
+public class BusinessException extends RuntimeException {
+    private final BusinessErrorCode businessErrorCode;
+
+    public BusinessException(BusinessErrorCode businessErrorCode) {
+        super(businessErrorCode.getMessage());
+        this.businessErrorCode = businessErrorCode;
+    }
+
+    public BusinessErrorCode getErrorCode() {
+        return businessErrorCode;
+    }
 }
+
