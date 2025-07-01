18ec3759e6c93e3303e6a33bb7815b14b1b7606c
hyeumm <20221159@sungshin.ac.kr>
2025-07-01 09:44:10 +0900

    [feat/#5] 스웨거 적용

--- src/main/java/at/mateball/domain/sample/api/dto/request/SampleRequest.java
+++ src/main/java/at/mateball/domain/sample/api/dto/request/SampleRequest.java
@@ -1,10 +1,13 @@
 package at.mateball.domain.sample.api.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record SampleRequest(
         @NotNull
+        @Schema(description = "어떤 필드인지 작성합니다")
         String data1,
+        @Schema(description = "어떤 필드인지 작성합니다", nullable = true)
         String data2
 ) {
 }
