0bb78edb10eb0e8d566d130f44254d874ed39b48
dajinYang <didekwls0104@naver.com>
2025-07-02 15:46:58 +0900

    [chore#13] redis bean 제거

--- src/main/java/at/mateball/config/RedisConfig.java
+++ src/main/java/at/mateball/config/RedisConfig.java
@@ -1,10 +1,7 @@
 package at.mateball.config;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -12,13 +9,6 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 @EnableRedisRepositories(basePackages = "at.mateball.domain.auth.core.repository")
 public class RedisConfig {
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
