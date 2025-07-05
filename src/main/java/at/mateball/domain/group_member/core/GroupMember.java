5d9a6ead9bc71a7b6402bd03c67927c0ce19d9e6
hyeumm <20221159@sungshin.ac.kr>
2025-07-05 15:52:20 +0900

    [feat/#19] 그룹원 엔티티 생성

---
+++ src/main/java/at/mateball/domain/group_member/core/GroupMember.java
@@ -0,0 +1,40 @@
+package at.mateball.domain.group_member.core;
+
+import at.mateball.domain.group.core.Group;
+import at.mateball.domain.user.core.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "group_member")
+public class GroupMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @Column(nullable = false)
+    private Boolean isParticipant = false;
+
+    @Column(nullable = false)
+    private Integer status = 3; // TODO: "승인 대기 중"==3 로 기본값 설정함. Integer 값 확정 필요
+
+    protected GroupMember() {
+    }
+
+    public GroupMember(User user, Group group, Boolean isParticipant, Integer status) {
+        this.user = user;
+        this.group = group;
+        this.isParticipant = isParticipant;
+        this.status = status;
+    }
+}
