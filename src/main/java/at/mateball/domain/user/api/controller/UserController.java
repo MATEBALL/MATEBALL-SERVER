743a7f9a7f38bfd597c09792a55998eff5e00f21
hyeumm <20221159@sungshin.ac.kr>
2025-07-08 05:46:45 +0900

    [chore/#39] 코드 리뷰 반영

--- src/main/java/at/mateball/domain/user/api/controller/UserController.java
+++ src/main/java/at/mateball/domain/user/api/controller/UserController.java
@@ -4,7 +4,7 @@ import at.mateball.common.MateballResponse;
 import at.mateball.common.security.CustomUserDetails;
 import at.mateball.common.swagger.CustomExceptionDescription;
 import at.mateball.common.swagger.SwaggerResponseDescription;
-import at.mateball.domain.user.api.dto.request.NicknameRequest;
+import at.mateball.domain.user.api.dto.request.NicknameReq;
 import at.mateball.domain.user.core.service.UserService;
 import at.mateball.exception.code.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,13 +27,13 @@ public class UserController {
     @PostMapping("/info/nickname")
     @CustomExceptionDescription(SwaggerResponseDescription.UPDATE_NICKNAME)
     @Operation(summary = "닉네임 설정 api")
-    public ResponseEntity<MateballResponse<Void>> updateNickname(
+    public ResponseEntity<MateballResponse<?>> updateNickname(
             @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody NicknameRequest nicknameRequest
+            @RequestBody NicknameReq nicknameReq
     ) {
         Long userId = userDetails.getUserId();
 
-        userService.updateNickname(userId, nicknameRequest.nickname());
+        userService.updateNickname(userId, nicknameReq.nickname());
 
         return ResponseEntity.ok(MateballResponse.successWithNoData(SuccessCode.CREATED));
     }
--- src/main/java/at/mateball/domain/user/api/dto/request/NicknameReq.java
+++ src/main/java/at/mateball/domain/user/api/dto/request/NicknameReq.java
@@ -2,9 +2,8 @@ package at.mateball.domain.user.api.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-public record NicknameRequest(
+public record NicknameReq(
         @NotBlank
         @Schema(description = "설정할 닉네임")
         String nickname
