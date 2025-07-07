29293ef0b94adc6a6ed991af20c6145dee85c842
dajinYang <didekwls0104@naver.com>
2025-07-07 21:40:31 +0900

    [feat/#28] 매칭률 로직 사용

--- src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementService.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementService.java
@@ -1,7 +1,10 @@
 package at.mateball.domain.matchrequirement.core.service;
 
+import at.mateball.domain.matchrequirement.api.dto.MatchingScoreDto;
 import at.mateball.domain.matchrequirement.core.repository.MatchRequirementRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MatchRequirementService {
@@ -9,5 +12,9 @@ public class MatchRequirementService {
 
     public MatchRequirementService(MatchRequirementRepository matchRequirementRepository) {
         this.matchRequirementRepository = matchRequirementRepository;
+    }
+
+    public List<MatchingScoreDto> getMatchings(Long userId) {
+        return matchRequirementRepository.findMatchingUsers(userId);
     }
 }
