b7df65a50a6223aab29f063acfee28a53c271896
dajinYang <didekwls0104@naver.com>
2025-07-08 01:20:12 +0900

    [feat/#35] getDirectMatching 컨트롤러 작성

--- src/main/java/at/mateball/domain/group/api/controller/GroupController.java
+++ src/main/java/at/mateball/domain/group/api/controller/GroupController.java
@@ -1,13 +1,32 @@
 package at.mateball.domain.group.api.controller;
 
+import at.mateball.common.MateballResponse;
+import at.mateball.common.security.CustomUserDetails;
+import at.mateball.domain.group.api.dto.DirectCreateRes;
 import at.mateball.domain.group.core.service.GroupService;
-import org.springframework.web.bind.annotation.RestController;
+import at.mateball.exception.code.SuccessCode;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1/users")
 public class GroupController {
     private final GroupService groupService;
 
     public GroupController(GroupService groupService) {
         this.groupService = groupService;
+    }
+
+    @GetMapping("/direct/{matchId}")
+    public ResponseEntity<MateballResponse<?>> getDirectMatching(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @NotNull @PathVariable Long matchId
+    ) {
+        Long userId = customUserDetails.getUserId();
+        DirectCreateRes directCreateRes = groupService.getDirectMatching(userId);
+
+        return ResponseEntity.ok(MateballResponse.success(SuccessCode.OK, directCreateRes));
     }
 }
--- src/main/java/at/mateball/domain/group/core/repository/querydsl/GroupRepositoryImpl.java
+++ src/main/java/at/mateball/domain/group/core/repository/querydsl/GroupRepositoryImpl.java
@@ -1,7 +1,15 @@
 package at.mateball.domain.group.core.repository.querydsl;
 
+import at.mateball.domain.gameinformation.core.QGameInformation;
+import at.mateball.domain.group.QGroup;
+import at.mateball.domain.group.api.dto.DirectCreateRes;
+import at.mateball.domain.matchrequirement.core.QMatchRequirement;
+import at.mateball.domain.user.core.QUser;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+
+import java.util.List;
 
 public class GroupRepositoryImpl implements GroupRepositoryCustom {
     private final JPAQueryFactory queryFactory;
@@ -11,4 +19,38 @@ public class GroupRepositoryImpl implements GroupRepositoryCustom {
         this.queryFactory = queryFactory;
         this.entityManager = entityManager;
     }
+
+    @Override
+    public List<DirectCreateRes> findDirectCreateResults(Long userId) {
+        QGroup group = QGroup.group;
+        QUser user = QUser.user;
+        QGameInformation gameInformation = QGameInformation.gameInformation;
+        QMatchRequirement matchRequirement = QMatchRequirement.matchRequirement;
+
+        List<Tuple> tuples = queryFactory
+                .select(
+                        group.id,
+                        user.nickname,
+                        user.birthYear,
+                        user.gender,
+                        matchRequirement.team,
+                        matchRequirement.style,
+                        gameInformation.awayTeamName,
+                        gameInformation.homeTeamName,
+                        gameInformation.stadiumName,
+                        gameInformation.gameDate,
+                        user.imgUrl
+                )
+                .from(group)
+                .join(user).on(group.id.eq(user.id))
+                .join(gameInformation).on(group.id.eq(gameInformation.id))
+                .join(matchRequirement).on(user.id.eq(matchRequirement.user.id))
+                .where(user.id.eq(userId))
+                .fetch();
+
+        return tuples.stream()
+                .map(DirectCreateRes::from)
+                .toList();
+    }
+
 }
