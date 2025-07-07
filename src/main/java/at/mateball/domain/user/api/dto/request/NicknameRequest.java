366c58fb0ee30007584ddbc35854c1c5b5744ded
hyeumm <20221159@sungshin.ac.kr>
2025-07-08 05:42:25 +0900

    [chore/#39] 코드 리뷰 반영

--- src/main/java/at/mateball/domain/user/api/dto/request/NicknameRequest.java
+++ src/main/java/at/mateball/domain/user/api/dto/request/NicknameRequest.java
@@ -1,10 +1,11 @@
 package at.mateball.domain.user.api.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record NicknameRequest(
-        @NotNull
+        @NotBlank
         @Schema(description = "설정할 닉네임")
         String nickname
 ) {
