e4a1cd9966c414dfc0575147f2275c9699a31d37
dajinYang <didekwls0104@naver.com>
2025-07-08 01:20:12 +0900

    [feat/#35] getDirectMatching 컨트롤러 작성

--- src/main/java/at/mateball/domain/group/core/repository/querydsl/GroupRepositoryCustom.java
+++ src/main/java/at/mateball/domain/group/core/repository/querydsl/GroupRepositoryCustom.java
@@ -1,7 +1,11 @@
 package at.mateball.domain.group.core.repository.querydsl;
 
+import at.mateball.domain.group.api.dto.DirectCreateRes;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GroupRepositoryCustom {
+    List<DirectCreateRes> findDirectCreateResults(Long userId);
 }
