099e213d3d64b36efe7b6d2d99dc5eb752106e11
dajinYang <didekwls0104@naver.com>
2025-07-09 00:31:15 +0900

    [feat/#54] 그룹 매칭 생성 결과 조회

--- src/main/java/at/mateball/domain/group/core/repository/querydsl/GroupRepositoryCustom.java
+++ src/main/java/at/mateball/domain/group/core/repository/querydsl/GroupRepositoryCustom.java
@@ -2,14 +2,18 @@ package at.mateball.domain.group.core.repository.querydsl;
 
 import at.mateball.domain.group.api.dto.DirectCreateRes;
 import at.mateball.domain.group.api.dto.DirectBaseRes;
+import at.mateball.domain.group.api.dto.GroupCreateRes;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GroupRepositoryCustom {
     DirectCreateRes findDirectCreateResults(Long userId, Long matchId);
 
     List<DirectBaseRes> findDirectGroupsByDate(LocalDate date);
+
+    Optional<GroupCreateRes> findGroupCreateRes(Long matchId);
 }
