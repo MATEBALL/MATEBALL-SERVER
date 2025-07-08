b31e12e01d4d9740eb95b4d19589e1e35b021618
hyeumm <20221159@sungshin.ac.kr>
2025-07-08 12:21:02 +0900

    [chore/#33] blank,null 처리 추가

--- src/main/java/at/mateball/domain/user/api/dto/response/KaKaoInformationRes.java
+++ src/main/java/at/mateball/domain/user/api/dto/response/KaKaoInformationRes.java
@@ -1,10 +1,14 @@
 package at.mateball.domain.user.api.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record KaKaoInformationRes(
+        @NotNull
         @Schema(description = "사용자의 생년월일")
         Integer birthYear,
+        @NotBlank
         @Schema(description = "사용자의 성별")
         String gender
 ) {
