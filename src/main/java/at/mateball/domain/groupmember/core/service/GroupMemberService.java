c6c6ff47f746de1debea72b102887f30acef06b1
dajinYang <didekwls0104@naver.com>
2025-07-09 06:57:46 +0900

    [chore/#64] 컨트롤러 수정

--- src/main/java/at/mateball/domain/groupmember/core/service/GroupMemberService.java
+++ src/main/java/at/mateball/domain/groupmember/core/service/GroupMemberService.java
@@ -3,7 +3,10 @@ package at.mateball.domain.groupmember.core.service;
 import at.mateball.domain.group.core.GroupStatus;
 import at.mateball.domain.groupmember.api.dto.DirectStatusListRes;
 import at.mateball.domain.groupmember.api.dto.DirectStatusRes;
+import at.mateball.domain.groupmember.api.dto.GroupStatusListRes;
+import at.mateball.domain.groupmember.api.dto.GroupStatusRes;
 import at.mateball.domain.groupmember.api.dto.base.DirectStatusBaseRes;
+import at.mateball.domain.groupmember.api.dto.base.GroupStatusBaseRes;
 import at.mateball.domain.groupmember.core.repository.GroupMemberRepository;
 import org.springframework.stereotype.Service;
 
@@ -30,10 +33,15 @@ public class GroupMemberService {
     public DirectStatusListRes getAllDirectStatus(Long userId) {
         List<DirectStatusBaseRes> baseResList = groupMemberRepository.findAllDirectMatchingsByUser(userId);
 
-        List<DirectStatusRes> mapped = baseResList.stream()
+        List<DirectStatusRes> result = baseResList.stream()
                 .map(DirectStatusRes::from)
                 .toList();
 
-        return new DirectStatusListRes(mapped);
+        return new DirectStatusListRes(result);
+    }
+
+    public GroupStatusListRes getAllGroupStatus(Long userId) {
+        List<GroupStatusBaseRes> baseResList = groupMemberRepository.findGroupMatchingsByUser(userId);
+
     }
 }
