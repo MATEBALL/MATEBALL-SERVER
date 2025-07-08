6eb6dfd1020435603acc0c8ddd73dec6374589dc
hyeumm <20221159@sungshin.ac.kr>
2025-07-08 11:32:13 +0900

    [feat/#33] 카카오 정보 조회 api 구현

---
+++ src/main/java/at/mateball/domain/user/api/dto/response/KaKaoInformationRes.java
@@ -0,0 +1,7 @@
+package at.mateball.domain.user.api.dto.response;
+
+public record KaKaoInformationRes(
+        Integer birthYear,
+        String gender
+) {
+}
--- src/main/java/at/mateball/domain/user/core/service/UserService.java
+++ src/main/java/at/mateball/domain/user/core/service/UserService.java
@@ -1,5 +1,7 @@
 package at.mateball.domain.user.core.service;
 
+import at.mateball.domain.matchrequirement.core.constant.Gender;
+import at.mateball.domain.user.api.dto.response.KaKaoInformationRes;
 import at.mateball.domain.user.core.User;
 import at.mateball.domain.user.core.repository.UserRepository;
 import at.mateball.exception.BusinessException;
@@ -8,14 +10,33 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static at.mateball.exception.code.BusinessErrorCode.DUPLICATED_NICKNAME;
-import static at.mateball.exception.code.BusinessErrorCode.USER_NOT_FOUND;
+import java.time.LocalDateTime;
+
+import static at.mateball.exception.code.BusinessErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+
+    public KaKaoInformationRes getKakaoInformation(final Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(USER_NOT_FOUND));
+
+        int currentYear = LocalDateTime.now().getYear();
+        int birthYear = user.getBirthYear();
+
+        int LIMIT_AGE = 19;
+        int age = currentYear - birthYear + 1;
+        if (age < LIMIT_AGE) {
+            throw new BusinessException(AGE_NOT_APPROPRIATE);
+        }
+
+        String gender = Gender.from(user.getGender()).getLabel();
+
+        return new KaKaoInformationRes(birthYear, gender);
+    }
 
     @Transactional
     public void updateNickname(final Long userId, final String updatedNickname) {
