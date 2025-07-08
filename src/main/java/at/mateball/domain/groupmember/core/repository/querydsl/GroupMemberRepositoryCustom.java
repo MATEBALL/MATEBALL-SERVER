5497f75d901033ead5f3f3260f69bfc698306742
dajinYang <didekwls0104@naver.com>
2025-07-09 05:29:16 +0900

    [feat/#62] 라벨 코드로 변환 로직 추가

--- src/main/java/at/mateball/domain/groupmember/core/repository/querydsl/GroupMemberRepositoryCustom.java
+++ src/main/java/at/mateball/domain/groupmember/core/repository/querydsl/GroupMemberRepositoryCustom.java
@@ -1,6 +1,8 @@
 package at.mateball.domain.groupmember.core.repository.querydsl;
 
 
+import at.mateball.domain.groupmember.api.dto.base.DirectStatusBaseRes;
+
 import java.util.List;
 import java.util.Map;
 
@@ -8,4 +10,6 @@ public interface GroupMemberRepositoryCustom {
     Map<Long, Integer> findGroupMemberCountMap(List<Long> groupIds);
 
     Map<Long, List<String>> findGroupMemberImgMap(List<Long> groupIds);
+
+    List<DirectStatusBaseRes> findDirectMatchingsByUserAndGroupStatus(Long userId, int groupStatus);
 }
