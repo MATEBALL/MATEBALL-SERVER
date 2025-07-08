4d199329da64934ced6dbce505c1f90776e02673
dajinYang <didekwls0104@naver.com>
2025-07-09 05:09:38 +0900

    [feat/#62] controller 생성

--- src/main/java/at/mateball/domain/groupmember/api/controller/GroupMemberController.java
+++ src/main/java/at/mateball/domain/groupmember/api/controller/GroupMemberController.java
@@ -1,13 +1,34 @@
 package at.mateball.domain.groupmember.api.controller;
 
+import at.mateball.common.MateballResponse;
+import at.mateball.common.security.CustomUserDetails;
+import at.mateball.domain.groupmember.api.dto.DirectStatusListRes;
 import at.mateball.domain.groupmember.core.service.GroupMemberService;
+import at.mateball.exception.code.SuccessCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/users")
 public class GroupMemberController {
     private final GroupMemberService groupMemberService;
 
     public GroupMemberController(GroupMemberService groupMemberService) {
         this.groupMemberService = groupMemberService;
+    }
+
+    @GetMapping("/match-stage/direct")
+    public ResponseEntity<MateballResponse<?>> getDirectStatus(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam String status
+    ) {
+        Long userId = userDetails.getUserId();
+        DirectStatusListRes directStatusListRes = groupMemberService.getDirectStatus(userId, status);
+
+        return ResponseEntity.ok(MateballResponse.success(SuccessCode.OK, directStatusListRes));
     }
 }
