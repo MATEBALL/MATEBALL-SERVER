795bbba8ed578f6a025fd341599bbee610e52346
hyeumm <20221159@sungshin.ac.kr>
2025-07-10 02:04:38 +0900

    [chore/#60] 메소드 순서 변경

--- src/main/java/at/mateball/domain/groupmember/core/repository/GroupMemberRepositoryCustom.java
+++ src/main/java/at/mateball/domain/groupmember/core/repository/GroupMemberRepositoryCustom.java
@@ -2,18 +2,23 @@ package at.mateball.domain.groupmember.core.repository;
 
 import at.mateball.domain.groupmember.core.GroupMemberStatus;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface GroupMemberRepositoryCustom {
-    void createGroupMember(Long userId, Long matchId);
+    boolean existsRequest(Long userId, Long groupId);
+
+    boolean isPendingRequestExists(Long matchId, List<Integer> status);
+
+    boolean hasNonFailedRequestOnSameDate(Long userId, LocalDate date);
+
+    boolean hasPreviousFailedRequest(Long userId, Long matchId, GroupMemberStatus status);
 
     long countMatchingRequests(Long userId, boolean isGroup);
+
+    void createGroupMember(Long userId, Long matchId);
 
     void updateLeaderStatus(Long userId, Long matchId, int status);
 
     void updateStatusForAllParticipants(Long matchId, int status);
-
-    boolean isPendingRequestExists(Long matchId, List<Integer> status);
-
-    boolean hasPreviousFailedRequest(Long userId, Long matchId, GroupMemberStatus status);
 }
--- src/main/java/at/mateball/exception/code/BusinessErrorCode.java
+++ src/main/java/at/mateball/exception/code/BusinessErrorCode.java
@@ -12,15 +12,15 @@ public enum BusinessErrorCode implements ErrorCode {
     KAKAO_USER_INFO_FETCH_FAILED(HttpStatus.BAD_REQUEST, "카카오 사용자 정보를 가져오는 데 실패했습니다."),
     BAD_REQUEST_ENUM(HttpStatus.BAD_REQUEST, "존재하지 않는 enum값입니다."),
     BAD_REQUEST_MONDAY(HttpStatus.BAD_REQUEST, "월요일은 경기가 없습니다."),
-    BAD_REQUEST_PAST(HttpStatus.BAD_REQUEST,"과거에 머물러있지 마십쇼. 이미 종료된 경기입니다."),
+    BAD_REQUEST_PAST(HttpStatus.BAD_REQUEST, "과거에 머물러있지 마십쇼. 이미 종료된 경기입니다."),
     BAD_REQUEST_DATE(HttpStatus.BAD_REQUEST, "매칭신청은 2일 전까지 가능합니다."),
     EXCEED_GROUP_MATCHING_LIMIT(HttpStatus.BAD_REQUEST, "그룹 매칭은 최대 2개까지만 가능합니다."),
     EXCEED_DIRECT_MATCHING_LIMIT(HttpStatus.BAD_REQUEST, "1대1 매칭은 최대 3개까지만 가능합니다."),
     ALREADY_HAS_PENDING_REQUEST(HttpStatus.BAD_REQUEST, "이미 요청이 존재하는 매칭입니다."),
-    ALREADY_FAILED_REQUEST(HttpStatus.BAD_REQUEST, "이미 요청이 실패한 매칭입니다."),
+    DUPLICATE_REQUEST_ON_SAME_DATE(HttpStatus.BAD_REQUEST, "같은 날짜의 경기에는 하나의 매칭 요청만 생성할 수 있습니다."),
 
     // 401 UNAUTHORIZED
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED,"만료된 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "Access Token이 유효하지 않습니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "Refresh Token이 유효하지 않습니다."),
     INVALID_KAKAO_TOKEN(HttpStatus.UNAUTHORIZED, "Kakao Access Token이 유효하지 않습니다."),
@@ -32,11 +32,13 @@ public enum BusinessErrorCode implements ErrorCode {
 
     // 404 NOT FOUND
     TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "요청에 해당하는 토큰이 존재하지 않습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 사용자입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 매칭입니다."),
 
     // 409 CONFLICT
-    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다.");
+    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다."),
+    ALREADY_FAILED_REQUEST(HttpStatus.CONFLICT, "이미 요청이 실패한 매칭입니다."),
+    DUPLICATED_REQUEST(HttpStatus.CONFLICT, "이미 요청을 전송한 매칭입니다.");
 
     private final HttpStatus status;
     private final String message;
