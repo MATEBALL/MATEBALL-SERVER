3e08aa02fd24b07f7cea4e0ea64421a5cc20ddac
dajinYang <didekwls0104@naver.com>
2025-07-09 19:52:11 +0900

    [feat/#66] Controller 생성

--- src/main/java/at/mateball/domain/groupmember/api/controller/GroupMemberController.java
+++ src/main/java/at/mateball/domain/groupmember/api/controller/GroupMemberController.java
@@ -3,16 +3,14 @@ package at.mateball.domain.groupmember.api.controller;
 import at.mateball.common.MateballResponse;
 import at.mateball.common.security.CustomUserDetails;
 import at.mateball.domain.group.core.GroupStatus;
+import at.mateball.domain.groupmember.api.dto.DetailMatchingListRes;
 import at.mateball.domain.groupmember.api.dto.DirectStatusListRes;
 import at.mateball.domain.groupmember.api.dto.GroupStatusListRes;
 import at.mateball.domain.groupmember.core.service.GroupMemberService;
 import at.mateball.exception.code.SuccessCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -57,5 +55,16 @@ public class GroupMemberController {
         }
 
         return ResponseEntity.ok(MateballResponse.success(SuccessCode.OK, result));
+    }
+
+    @GetMapping("/match-detail")
+    public ResponseEntity<MateballResponse<?>> getDetailMatching(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long matchId
+    ) {
+        Long userId = userDetails.getUserId();
+        DetailMatchingListRes detailMatchingListRes = groupMemberService.getDetailMatching(userId, matchId);
+
+        return ResponseEntity.ok(MateballResponse.success(SuccessCode.OK, detailMatchingListRes));
     }
 }
