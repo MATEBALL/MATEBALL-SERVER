ff1e4b8cac4a54024eef39ef3cd748014403d129
hyeumm <20221159@sungshin.ac.kr>
2025-07-11 05:35:14 +0900

    [chore/#83] date validator 적용

--- src/main/java/at/mateball/domain/gameinformation/core/service/GameInformationService.java
+++ src/main/java/at/mateball/domain/gameinformation/core/service/GameInformationService.java
@@ -2,11 +2,7 @@ package at.mateball.domain.gameinformation.core.service;
 
 import at.mateball.domain.gameinformation.api.dto.response.GameInformationRes;
 import at.mateball.domain.gameinformation.api.dto.response.GameInformationsRes;
-import at.mateball.domain.gameinformation.core.GameInformation;
 import at.mateball.domain.gameinformation.core.repository.GameInformationRepository;
-import at.mateball.domain.user.core.User;
-import at.mateball.domain.user.core.repository.UserRepository;
-import at.mateball.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,16 +10,16 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-import static at.mateball.exception.code.BusinessErrorCode.USER_NOT_FOUND;
+import static at.mateball.domain.group.core.validator.DateValidator.validate;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class GameInformationService {
-    private final UserRepository userRepository;
     private final GameInformationRepository gameInformationRepository;
 
     public GameInformationsRes getGameInformation(Long userId, LocalDate date) {
+        validate(date);
 
         List<GameInformationRes> list = gameInformationRepository.findByGameDate(date)
                 .stream()
