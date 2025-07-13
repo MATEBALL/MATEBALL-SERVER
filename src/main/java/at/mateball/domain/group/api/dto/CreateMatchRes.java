1514ee2dd56077e8a5ee919096ba211b1d61a23a
hyeumm <20221159@sungshin.ac.kr>
2025-07-14 03:42:34 +0900

    [chore/#98] 네이밍 통일

--- src/main/java/at/mateball/domain/group/api/dto/CreateMatchRes.java
+++ src/main/java/at/mateball/domain/group/api/dto/CreateMatchRes.java
@@ -3,7 +3,7 @@ package at.mateball.domain.group.api.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreateMatchRes(
-        @Schema(description = "그룹 ID")
+        @Schema(description = "매칭 ID")
         Long matchId
 ) {
 }
