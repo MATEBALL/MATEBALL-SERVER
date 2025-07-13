e7618ec16f1584c6e41c664d70896167af689bb5
hyeumm <20221159@sungshin.ac.kr>
2025-07-14 07:03:55 +0900

    [chore/#102] 매칭 조건이 null일 때 분기처리

--- src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementService.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementService.java
@@ -7,6 +7,8 @@ import at.mateball.domain.matchrequirement.core.constant.Style;
 import at.mateball.domain.matchrequirement.core.constant.TeamAllowed;
 import at.mateball.domain.matchrequirement.core.repository.MatchRequirementRepository;
 import at.mateball.domain.team.core.TeamName;
+import at.mateball.domain.user.core.User;
+import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,9 +18,11 @@ import java.util.List;
 public class MatchRequirementService {
     private final MatchRequirementRepository matchRequirementRepository;
 
-    public MatchRequirementService(
-            MatchRequirementRepository matchRequirementRepository) {
+    private final EntityManager entityManager;
+
+    public MatchRequirementService(MatchRequirementRepository matchRequirementRepository, EntityManager entityManager) {
         this.matchRequirementRepository = matchRequirementRepository;
+        this.entityManager = entityManager;
     }
 
     public List<MatchingScoreDto> getMatchings(Long userId) {
@@ -27,13 +31,25 @@ public class MatchRequirementService {
 
     @Transactional
     public void setMatchRequirement(Long userId, String team, String teamAllowed, String style, String genderPreference) {
+        User user = entityManager.getReference(User.class, userId);
         MatchRequirement matchRequirement = matchRequirementRepository.findUserMatchRequirement(userId);
 
-        matchRequirement.updateAll(
-                TeamName.fromLabel(team).getValue(),
-                teamAllowed == null ? TeamAllowed.NO_PREFERENCE.getValue() : TeamAllowed.fromLabel(teamAllowed).getValue(),
-                Style.fromLabel(style).getValue(),
-                Gender.fromLabel(genderPreference).getValue()
-        );
+        if (matchRequirement == null) {
+            matchRequirement = new MatchRequirement(
+                    user,
+                    TeamName.fromLabel(team).getValue(),
+                    teamAllowed == null ? TeamAllowed.NO_PREFERENCE.getValue() : TeamAllowed.fromLabel(teamAllowed).getValue(),
+                    Style.fromLabel(style).getValue(),
+                    Gender.fromLabel(genderPreference).getValue()
+            );
+            entityManager.persist(matchRequirement);
+        } else {
+            matchRequirement.updateAll(
+                    TeamName.fromLabel(team).getValue(),
+                    teamAllowed == null ? TeamAllowed.NO_PREFERENCE.getValue() : TeamAllowed.fromLabel(teamAllowed).getValue(),
+                    Style.fromLabel(style).getValue(),
+                    Gender.fromLabel(genderPreference).getValue()
+            );
+        }
     }
 }
