c381001d7f1d717d4bc47eff33dd9468d844905c
dajinYang <didekwls0104@naver.com>
2025-07-14 00:26:14 +0900

    [feat/#95] 카카오 로그인 스웨거 연동

--- src/main/java/at/mateball/domain/auth/api/dto/LoginCommand.java
+++ src/main/java/at/mateball/domain/auth/api/dto/LoginCommand.java
@@ -1,6 +1,11 @@
 package at.mateball.domain.auth.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 public record LoginCommand(
+        @NotNull
+        @Schema(description = "인가 코드")
         String code
 ) {
 }
