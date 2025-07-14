d910d87c151f48023064cf801bfecbc0e16a3ad3
hyeumm <20221159@sungshin.ac.kr>
2025-07-14 14:57:22 +0900

    [chore/#102] 나이 계산 utils 생성 및 적용

--- src/main/java/at/mateball/domain/group/api/dto/DirectCreateRes.java
+++ src/main/java/at/mateball/domain/group/api/dto/DirectCreateRes.java
@@ -1,19 +1,14 @@
 package at.mateball.domain.group.api.dto;
 
-import at.mateball.domain.gameinformation.core.QGameInformation;
 import at.mateball.domain.group.api.dto.base.DirectCreateBaseRes;
-import at.mateball.domain.group.core.QGroup;
-import at.mateball.domain.matchrequirement.core.QMatchRequirement;
 import at.mateball.domain.matchrequirement.core.constant.Gender;
 import at.mateball.domain.matchrequirement.core.constant.Style;
 import at.mateball.domain.team.core.TeamName;
-import at.mateball.domain.user.core.QUser;
-import com.querydsl.core.Tuple;
+import at.mateball.util.AgeUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public record DirectCreateRes(
         @NotNull
@@ -49,26 +44,26 @@ public record DirectCreateRes(
         @Schema(description = "이미지 URL", nullable = true)
         String imgUrl
 ) {
-        public static DirectCreateRes from(DirectCreateBaseRes baseRes) {
-                String age = null;
+    public static DirectCreateRes from(DirectCreateBaseRes baseRes) {
+        String age = null;
 
-                if (baseRes.birthYear() != null) {
-                        age = (LocalDate.now().getYear() - baseRes.birthYear() + 1) + "세";
-                }
-
-                return new DirectCreateRes(
-                        baseRes.id(),
-                        baseRes.nickname(),
-                        age,
-                        Gender.from(baseRes.gender()).getLabel(),
-                        TeamName.from(Integer.parseInt(baseRes.team().toString())).getLabel(),
-                        Style.from(baseRes.style()).getLabel(),
-                        baseRes.awayTeam(),
-                        baseRes.homeTeam(),
-                        baseRes.stadium(),
-                        baseRes.date(),
-                        baseRes.imgUrl()
-                );
+        if (baseRes.birthYear() != null) {
+            age = AgeUtils.calculateAge(baseRes.birthYear());
         }
+
+        return new DirectCreateRes(
+                baseRes.id(),
+                baseRes.nickname(),
+                age,
+                Gender.from(baseRes.gender()).getLabel(),
+                TeamName.from(Integer.parseInt(baseRes.team().toString())).getLabel(),
+                Style.from(baseRes.style()).getLabel(),
+                baseRes.awayTeam(),
+                baseRes.homeTeam(),
+                baseRes.stadium(),
+                baseRes.date(),
+                baseRes.imgUrl()
+        );
+    }
 
 }
