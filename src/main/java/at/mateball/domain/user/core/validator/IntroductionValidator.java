78e4d9e2ca9b2db7bd0cdcbfdbbbcb4927b0ef40
hyeumm <20221159@sungshin.ac.kr>
2025-08-27 02:51:32 +0900

    [feat/#152] IntroductionValidator 구현

---
+++ src/main/java/at/mateball/domain/user/core/validator/IntroductionValidator.java
@@ -0,0 +1,15 @@
+package at.mateball.domain.user.core.validator;
+
+import at.mateball.exception.BusinessException;
+import at.mateball.exception.code.BusinessErrorCode;
+
+public class IntroductionValidator {
+    private static final Integer MIN_INTRODUCTION_LENGTH = 1;
+    private static final Integer MAX_INTRODUCTION_LENGTH = 50;
+
+    public static void validate(String introduction) {
+        if (introduction == null || introduction.isBlank() || introduction.length() < MIN_INTRODUCTION_LENGTH || introduction.length() > MAX_INTRODUCTION_LENGTH) {
+            throw new BusinessException(BusinessErrorCode.INVALID_NICKNAME_LENGTH);
+        }
+    }
+}
