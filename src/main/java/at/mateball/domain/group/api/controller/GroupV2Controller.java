61003637905bc6746cdc013c152048eca0c9b0bb
hyeumm <20221159@sungshin.ac.kr>
2025-08-26 23:04:01 +0900

    [feat/#144] 오픈채팅방 주소 조회 api 구현

--- src/main/java/at/mateball/domain/group/api/controller/GroupV2Controller.java
+++ src/main/java/at/mateball/domain/group/api/controller/GroupV2Controller.java
@@ -2,8 +2,11 @@ package at.mateball.domain.group.api.controller;
 
 import at.mateball.common.MateballResponse;
 import at.mateball.common.security.CustomUserDetails;
+<<<<<<< HEAD
 import at.mateball.common.swagger.CustomExceptionDescription;
 import at.mateball.common.swagger.SwaggerResponseDescription;
+=======
+>>>>>>> e596730 ([feat/#144] 오픈채팅방 주소 조회 api 구현)
 import at.mateball.domain.chatting.api.dto.response.ChattingRes;
 import at.mateball.domain.group.core.service.GroupV2Service;
 import at.mateball.exception.code.SuccessCode;
@@ -11,7 +14,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.*;
+=======
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+>>>>>>> e596730 ([feat/#144] 오픈채팅방 주소 조회 api 구현)
 
 @RestController
 @RequestMapping("/v2/users")
@@ -23,7 +33,11 @@ public class GroupV2Controller {
         this.groupV2Service = groupV2Service;
     }
 
+<<<<<<< HEAD
     @GetMapping("/match/{matchId}/chatting")
+=======
+    @GetMapping("/group/{matchId}/chatting")
+>>>>>>> e596730 ([feat/#144] 오픈채팅방 주소 조회 api 구현)
     @Operation(summary = "오픈채팅방 주소 조회 api")
     public ResponseEntity<MateballResponse<?>> getChattingUrl(
             @AuthenticationPrincipal CustomUserDetails userDetails,
