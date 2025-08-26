47cc40a3da49f5b71aa2998db7443950d34d3f50
dajinYang <didekwls0104@naver.com>
2025-08-27 02:47:16 +0900

    [fix/#150] 매칭완료시에 새요청은 자동으로 읽음 처리

--- src/main/java/at/mateball/domain/alarm/AlarmRepository.java
+++ src/main/java/at/mateball/domain/alarm/AlarmRepository.java
@@ -1,6 +1,9 @@
 package at.mateball.domain.alarm;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,4 +11,10 @@ import java.util.List;
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
     List<Alarm> findByUserIdAndIsReadFalse(Long userId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Alarm a SET a.isRead = true " +
+            "WHERE a.user.id = :userId AND a.type IN :types AND a.isRead = false")
+    void markAsReadByUserAndTypes(@Param("userId") Long userId,
+                                  @Param("types") List<AlarmType> types);
 }
