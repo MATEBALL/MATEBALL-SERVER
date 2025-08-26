8049bb29d5430fc4e213acc851736e3000023142
hyeumm <20221159@sungshin.ac.kr>
2025-08-27 02:26:59 +0900

    [feat/#152] 온보딩 회원 정보 설정 api 구현

--- src/main/java/at/mateball/domain/user/core/service/UserV2Service.java
+++ src/main/java/at/mateball/domain/user/core/service/UserV2Service.java
@@ -2,15 +2,24 @@ package at.mateball.domain.user.core.service;
 
 import at.mateball.domain.alarm.core.service.AlarmService;
 import at.mateball.domain.matchrequirement.core.constant.Gender;
+<<<<<<< HEAD
 import at.mateball.domain.user.api.dto.request.EditUserInfoReq;
+=======
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
 import at.mateball.domain.user.api.dto.request.UserInfoV2Req;
 import at.mateball.domain.user.api.dto.response.InfoCheckRes;
 import at.mateball.domain.user.core.User;
 import at.mateball.domain.user.core.UserInfoField;
 import at.mateball.domain.user.core.repository.UserRepository;
+<<<<<<< HEAD
 import at.mateball.domain.user.core.validator.IntroductionValidator;
 import at.mateball.domain.user.core.validator.NicknameValidator;
 import at.mateball.exception.BusinessException;
+=======
+import at.mateball.domain.user.core.validator.NicknameValidator;
+import at.mateball.exception.BusinessException;
+import at.mateball.exception.code.BusinessErrorCode;
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +34,11 @@ public class UserV2Service {
     private final AlarmService alarmService;
 
     private static final Integer LIMIT_AGE = 19;
+<<<<<<< HEAD
+=======
+    private static final Integer MIN_INTRODUCTION_LENGTH = 1;
+    private static final Integer MAX_INTRODUCTION_LENGTH = 50;
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
     private static final String DEFAULT_PROFILE_IMAGE_URL =
             "https://mateball-file.s3.ap-northeast-2.amazonaws.com/profile.jpg";
 
@@ -54,10 +68,13 @@ public class UserV2Service {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(USER_NOT_FOUND));
 
+<<<<<<< HEAD
         if (user.getNickname() != null || user.getIntroduction() != null || user.getBirthYear() != null || user.getGender() != null) {
             throw new BusinessException(DUPLICATED_INFO);
         }
 
+=======
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
         validateNickname(req.nickname());
         validateIntroduction(req.introduction());
         validateAge(req.birthYear());
@@ -68,6 +85,7 @@ public class UserV2Service {
         user.updateProfileImage(DEFAULT_PROFILE_IMAGE_URL);
     }
 
+<<<<<<< HEAD
     @Transactional
     public void editUserInfo(Long userId, @Valid EditUserInfoReq req) {
         User user = userRepository.findById(userId)
@@ -87,6 +105,8 @@ public class UserV2Service {
         }
     }
 
+=======
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
     private void validateNickname(String nickname) {
         NicknameValidator.validate(nickname);
         if (userRepository.existsByNickname(nickname)) {
@@ -95,7 +115,13 @@ public class UserV2Service {
     }
 
     private void validateIntroduction(String introduction) {
+<<<<<<< HEAD
         IntroductionValidator.validate(introduction);
+=======
+        if (introduction == null || introduction.isBlank() || introduction.length() < MIN_INTRODUCTION_LENGTH || introduction.length() > MAX_INTRODUCTION_LENGTH) {
+            throw new BusinessException(BusinessErrorCode.INVALID_INTRODUCTION_LENGTH);
+        }
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
     }
 
     private void validateAge(int birthYear) {
