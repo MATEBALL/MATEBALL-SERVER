939a208ea8fac9d0fcb2d773f93a26857789bc9c
hyeumm <20221159@sungshin.ac.kr>
2025-08-27 02:26:59 +0900

    [feat/#152] 온보딩 회원 정보 설정 api 구현

--- src/main/java/at/mateball/domain/user/api/dto/request/UserInfoV2Req.java
+++ src/main/java/at/mateball/domain/user/api/dto/request/UserInfoV2Req.java
@@ -10,7 +10,11 @@ public record UserInfoV2Req(
         String introduction,
 
         @NotNull
+<<<<<<< HEAD
         Integer birthYear,
+=======
+        int birthYear,
+>>>>>>> 29c4d6b ([feat/#152] 온보딩 회원 정보 설정 api 구현)
 
         @NotNull
         String gender
