f61676e9975cd5ce922c878512c79c82f79055ee
dajinYang <didekwls0104@naver.com>
2025-08-26 22:06:42 +0900

    [feat/#146] 약관 동의 상태 변환

--- src/main/java/at/mateball/domain/user/core/service/UserV2Service.java
+++ src/main/java/at/mateball/domain/user/core/service/UserV2Service.java
@@ -1,8 +1,12 @@
 package at.mateball.domain.user.core.service;
 
 import at.mateball.domain.user.api.dto.response.InfoCheckRes;
+import at.mateball.domain.user.core.User;
 import at.mateball.domain.user.core.repository.UserRepository;
+import at.mateball.exception.BusinessException;
+import at.mateball.exception.code.BusinessErrorCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserV2Service {
@@ -14,5 +18,17 @@ public class UserV2Service {
 
     public InfoCheckRes getInfoCheck(Long userId) {
         return userRepository.infoCheck(userId);
+    }
+
+    @Transactional
+    public void updateHasAccepted(Long userId, boolean hasAccepted) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(BusinessErrorCode.USER_NOT_FOUND));
+
+        if (user.isHasAccepted() == hasAccepted) {
+            return;
+        }
+
+        user.updateHasAccepted(hasAccepted);
     }
 }
