72f9e40a18f81afa145ef9d08931554c0c86d2c1
dajinYang <didekwls0104@naver.com>
2025-08-26 21:51:06 +0900

    [chore/#146] dto 이름 변경

--- src/main/java/at/mateball/domain/user/api/controller/UserV2Controller.java
+++ src/main/java/at/mateball/domain/user/api/controller/UserV2Controller.java
@@ -14,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/v2/users")
