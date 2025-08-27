53a11bdfd44d3bd02322ddf0ee9c0c4abc88dcde
dajinYang <didekwls0104@naver.com>
2025-08-27 15:02:57 +0900

    [fix/#150] 알림 상태 분리

--- src/main/java/at/mateball/domain/user/api/dto/response/InfoCheckRes.java
+++ src/main/java/at/mateball/domain/user/api/dto/response/InfoCheckRes.java
@@ -3,7 +3,6 @@ package at.mateball.domain.user.api.dto.response;
 public record InfoCheckRes(
         boolean nickname,
         boolean condition,
-        boolean hasAccepted,
-        boolean isAlarm
+        boolean hasAccepted
 ) {
 }
