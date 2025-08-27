5e24c87d5b31b6140063ad6edd11392474f9308f
hyeumm <20221159@sungshin.ac.kr>
2025-08-28 03:04:19 +0900

    [chore/#152] 타입 변경

--- src/main/java/at/mateball/domain/user/core/validator/IntroductionValidator.java
+++ src/main/java/at/mateball/domain/user/core/validator/IntroductionValidator.java
@@ -4,8 +4,8 @@ import at.mateball.exception.BusinessException;
 import at.mateball.exception.code.BusinessErrorCode;
 
 public class IntroductionValidator {
-    private static final Integer MIN_INTRODUCTION_LENGTH = 1;
-    private static final Integer MAX_INTRODUCTION_LENGTH = 50;
+    private static final int MIN_INTRODUCTION_LENGTH = 1;
+    private static final int MAX_INTRODUCTION_LENGTH = 50;
 
     public static void validate(String introduction) {
         if (introduction == null || introduction.isBlank() || introduction.length() < MIN_INTRODUCTION_LENGTH || introduction.length() > MAX_INTRODUCTION_LENGTH) {
