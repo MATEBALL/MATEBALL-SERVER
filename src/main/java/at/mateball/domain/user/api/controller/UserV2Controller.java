84779e08a6a8a4f1042a6c1a53ba7c550f27277f
dajinYang <didekwls0104@naver.com>
2025-08-28 04:38:21 +0900

    [chore/#157] controller 오타 수정

--- src/main/java/at/mateball/domain/user/api/controller/UserV2Controller.java
+++ src/main/java/at/mateball/domain/user/api/controller/UserV2Controller.java
@@ -17,10 +17,10 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/v2/users")
-public class UserV2Contrller {
+public class UserV2Controller {
     private final UserV2Service userV2Service;
 
-    public UserV2Contrller(UserV2Service userV2Service) {
+    public UserV2Controller(UserV2Service userV2Service) {
         this.userV2Service = userV2Service;
     }
 
