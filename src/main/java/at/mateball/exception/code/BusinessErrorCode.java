b95311e619cd013b4447a7b4413d8931177fcdda
dajinYang <didekwls0104@naver.com>
2025-08-28 01:50:11 +0900

    [chore/#150] 중복 알림 제거

--- src/main/java/at/mateball/exception/code/BusinessErrorCode.java
+++ src/main/java/at/mateball/exception/code/BusinessErrorCode.java
@@ -47,7 +47,6 @@ public enum BusinessErrorCode implements ErrorCode {
     MATCH_REQUIREMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "매칭 조건 정보가 존재하지 않습니다."),
     CHATTING_NOT_FOUND(HttpStatus.NOT_FOUND, "오픈채팅이 존재하지 않습니다."),
 
-    ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 알람입니다."),
     // 409 CONFLICT
     DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다."),
     ALREADY_FAILED_REQUEST(HttpStatus.CONFLICT, "이미 요청이 실패한 매칭입니다."),
