f0353a3f3a2f4fba6f8ba0570de7f92e18dd1394
YangDaJin0104 <didekwls0104@naver.com>
2026-03-08 22:22:05 +0900

    [fix/#202] 설정 계층에서 검증 추가

--- src/main/java/at/mateball/storage/S3Properties.java
+++ src/main/java/at/mateball/storage/S3Properties.java
@@ -1,15 +1,22 @@
 package at.mateball.storage;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@Validated
 @Configuration
 @ConfigurationProperties(prefix = "spring.cloud.aws.s3")
 public class S3Properties {
+
+    @NotBlank
     private String bucket;
+
+    @NotBlank
     private String defaultProfileKey;
 }
