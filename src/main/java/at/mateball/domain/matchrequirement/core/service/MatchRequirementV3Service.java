c922d0bc45582a06095a26a79105634a9f3bfb58
hyeumm <20221159@sungshin.ac.kr>
2026-03-11 01:04:35 +0900

    [feat/#211] setMatchRequirement-매칭조건 설정 로직 추가

--- src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
@@ -1,8 +1,16 @@
 package at.mateball.domain.matchrequirement.core.service;
 
+import at.mateball.domain.matchrequirement.core.MatchRequirement;
+import at.mateball.domain.matchrequirement.core.constant.Style;
+import at.mateball.domain.matchrequirement.core.constant.TeamAllowed;
 import at.mateball.domain.matchrequirement.core.repository.MatchRequirementRepository;
+import at.mateball.domain.team.core.TeamName;
+import at.mateball.domain.user.core.User;
+import at.mateball.exception.BusinessException;
+import at.mateball.exception.code.BusinessErrorCode;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MatchRequirementV3Service {
@@ -15,5 +23,23 @@ public class MatchRequirementV3Service {
         this.entityManager = entityManager;
     }
 
+    @Transactional
+    public void setMatchRequirement(Long userId, String team, String teamAllowed, int avgSeason, String style) {
+        User user = entityManager.getReference(User.class, userId);
+        MatchRequirement matchRequirement = matchRequirementRepository.findUserMatchRequirement(userId);
 
+        if (matchRequirement != null) {
+            throw new BusinessException(BusinessErrorCode.DUPLICATED_MATCH_REQUIREMENT);
+        }
+
+        user.updateAvgSeason(avgSeason);
+
+        matchRequirement = new MatchRequirement(
+                user,
+                TeamName.fromLabel(team).getValue(),
+                teamAllowed == null ? TeamAllowed.NO_PREFERENCE.getValue() : TeamAllowed.fromLabel(teamAllowed).getValue(),
+                Style.fromLabel(style).getValue()
+        );
+        entityManager.persist(matchRequirement);
+    }
 }
