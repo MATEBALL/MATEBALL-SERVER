5ecd9fe4971b70e41561d14b733e64be27f1d52e
hyeumm <20221159@sungshin.ac.kr>
2026-03-11 01:28:37 +0900

    [chore/#211] setAvgSeason 로직 추가

--- src/main/java/at/mateball/domain/user/core/service/UserV3Service.java
+++ src/main/java/at/mateball/domain/user/core/service/UserV3Service.java
@@ -1,13 +1,35 @@
 package at.mateball.domain.user.core.service;
 
+import at.mateball.domain.user.core.User;
 import at.mateball.domain.user.core.repository.UserRepository;
+import at.mateball.exception.BusinessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static at.mateball.exception.code.BusinessErrorCode.INVALID_AVG_SEASON;
+import static at.mateball.exception.code.BusinessErrorCode.USER_NOT_FOUND;
 
 @Service
 public class UserV3Service {
     private final UserRepository userRepository;
+    private final int MIN_AVG_SEASON = 0;
+    private final int MAX_AVG_SEASON = 999;
 
     public UserV3Service(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void setAvgSeason(final Long userId, final int avgSeason) {
+        if (avgSeason > MAX_AVG_SEASON || avgSeason < MIN_AVG_SEASON) {
+            throw new BusinessException(INVALID_AVG_SEASON);
+        }
+
+        findUser(userId).updateAvgSeason(avgSeason);
+    }
+
+    public User findUser(final Long userId) {
+        return userRepository.getUser(userId).orElseThrow(()
+                -> new BusinessException(USER_NOT_FOUND));
     }
 }
