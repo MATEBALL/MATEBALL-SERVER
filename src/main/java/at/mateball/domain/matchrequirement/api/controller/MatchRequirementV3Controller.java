a5aedccb2adc509ca280cca8e1748d361c6e715e
hyeumm <20221159@sungshin.ac.kr>
2026-03-11 01:05:26 +0900

    [feat/#211] setMatchRequirement -매칭조건 설정 api 추가

--- src/main/java/at/mateball/domain/matchrequirement/api/controller/MatchRequirementV3Controller.java
+++ src/main/java/at/mateball/domain/matchrequirement/api/controller/MatchRequirementV3Controller.java
@@ -1,7 +1,16 @@
 package at.mateball.domain.matchrequirement.api.controller;
 
+import at.mateball.common.MateballResponse;
+import at.mateball.common.security.CustomUserDetails;
+import at.mateball.domain.matchrequirement.api.dto.request.MatchRequirementV3Req;
 import at.mateball.domain.matchrequirement.core.service.MatchRequirementV3Service;
 import at.mateball.domain.user.core.repository.UserRepository;
+import at.mateball.exception.code.SuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +23,22 @@ public class MatchRequirementV3Controller {
         this.matchRequirementV3Service = matchRequirementV3Service;
     }
 
+    @PostMapping
+    @Operation(summary = "매칭 조건 설정 api")
+    public ResponseEntity<MateballResponse<?>> setMatchRequirement(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody MatchRequirementV3Req matchRequirementV3Req
+    ) {
+        Long userId = userDetails.getUserId();
 
+        matchRequirementV3Service.setMatchRequirement(
+                userId,
+                matchRequirementV3Req.team(),
+                matchRequirementV3Req.teamAllowed(),
+                matchRequirementV3Req.avgSeason(),
+                matchRequirementV3Req.style()
+        );
+
+        return ResponseEntity.ok(MateballResponse.successWithNoData(SuccessCode.CREATED));
+    }
 }
