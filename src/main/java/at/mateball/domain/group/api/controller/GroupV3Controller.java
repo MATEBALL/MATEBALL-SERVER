3a31cadc9ac7d7f6ae852583c419d45dc2d26e6b
YangDaJin0104 <didekwls0104@naver.com>
2026-03-10 21:36:45 +0900

    [feat/#208] 게임별 매칭 정보 반환 api 생성

--- src/main/java/at/mateball/domain/group/api/controller/GroupV3Controller.java
+++ src/main/java/at/mateball/domain/group/api/controller/GroupV3Controller.java
@@ -2,8 +2,8 @@ package at.mateball.domain.group.api.controller;
 
 import at.mateball.common.MateballResponse;
 import at.mateball.common.security.CustomUserDetails;
-import at.mateball.domain.group.api.dto.GroupMemberMatchingRateRes;
-import at.mateball.domain.group.core.service.GroupMatchingQueryService;
+import at.mateball.domain.group.api.dto.GroupMatchRes;
+import at.mateball.domain.group.core.service.GroupV3Service;
 import at.mateball.exception.code.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -11,28 +11,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v3/users")
 public class GroupV3Controller {
 
-    private final GroupMatchingQueryService groupMatchingQueryService;
+    private final GroupV3Service groupV3Service;
 
-    @GetMapping("/groups/{groupId}/match-rate")
-    @Operation(summary = "로그인 사용자 기준 그룹원별 매칭률 조회 API")
-    public ResponseEntity<MateballResponse<List<GroupMemberMatchingRateRes>>> getGroupMemberMatchingRates(
+    @GetMapping("/match/{gameId}")
+    @Operation(summary = "특정 경기 기준 매칭 가능한 그룹 및 개인 목록 조회 API")
+    public ResponseEntity<MateballResponse<GroupMatchRes>> getAvailableMatches(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
-            @PathVariable Long groupId
+            @PathVariable Long gameId
     ) {
         Long userId = customUserDetails.getUserId();
 
-        List<GroupMemberMatchingRateRes> response =
-                groupMatchingQueryService.getGroupMemberMatchingRates(userId, groupId);
+        GroupMatchRes result = groupV3Service.getGroupMatchs(userId, gameId);
 
-        return ResponseEntity.ok(
-                MateballResponse.success(SuccessCode.OK, response)
-        );
+        return ResponseEntity.ok(MateballResponse.success(SuccessCode.OK, result));
+
     }
 }
---
+++ src/main/java/at/mateball/domain/group/core/service/GroupV3Service.java
@@ -0,0 +1,13 @@
+package at.mateball.domain.group.core.service;
+
+import at.mateball.domain.group.api.dto.GroupMatchRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GroupV3Service {
+    public GroupMatchRes getGroupMatchs(Long userId, Long matchId) {
+
+    }
+}
