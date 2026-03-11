b1d7710778a8babe1c67eaa25f9bbed4602bf26e
hyeumm <20221159@sungshin.ac.kr>
2026-03-11 23:37:05 +0900

    [chore/#211] avgSeason의 입력값 제한을 entity에서 제한하도록 변경

--- src/main/java/at/mateball/domain/user/core/service/UserV3Service.java
+++ src/main/java/at/mateball/domain/user/core/service/UserV3Service.java
@@ -6,21 +6,14 @@ import at.mateball.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static at.mateball.exception.code.BusinessErrorCode.INVALID_AVG_SEASON;
 import static at.mateball.exception.code.BusinessErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class UserV3Service {
     private final UserRepository userRepository;
-    private static final int MIN_AVG_SEASON = 0;
-    private static final int MAX_AVG_SEASON = 999;
 
     public void setAvgSeason(final Long userId, final int avgSeason) {
-        if (avgSeason > MAX_AVG_SEASON || avgSeason < MIN_AVG_SEASON) {
-            throw new BusinessException(INVALID_AVG_SEASON);
-        }
-
         findUser(userId).updateAvgSeason(avgSeason);
     }
 
