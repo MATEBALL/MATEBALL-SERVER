b62866aebd92d20c611a9df209eb0d7a35e52fb8
hyeumm <20221159@sungshin.ac.kr>
2026-03-11 23:32:19 +0900

    [chore/#211] controller에서 단일 service만 호출하도록 변경

--- src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
@@ -6,24 +6,28 @@ import at.mateball.domain.matchrequirement.core.constant.TeamAllowed;
 import at.mateball.domain.matchrequirement.core.repository.MatchRequirementRepository;
 import at.mateball.domain.team.core.TeamName;
 import at.mateball.domain.user.core.User;
+import at.mateball.domain.user.core.service.UserV3Service;
 import at.mateball.exception.BusinessException;
 import at.mateball.exception.code.BusinessErrorCode;
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+
 public class MatchRequirementV3Service {
     private final MatchRequirementRepository matchRequirementRepository;
-
+    private final UserV3Service userV3Service;
     private final EntityManager entityManager;
 
-    public MatchRequirementV3Service(MatchRequirementRepository matchRequirementRepository, EntityManager entityManager) {
-        this.matchRequirementRepository = matchRequirementRepository;
-        this.entityManager = entityManager;
+    @Transactional
+    public void setAvgSeasonAndMatchRequirement(Long userId, String team, String teamAllowed, String style, int avgSeason) {
+        userV3Service.setAvgSeason(userId, avgSeason);
+        setMatchRequirement(userId, team, teamAllowed, style);
     }
 
-    @Transactional
     public void setMatchRequirement(Long userId, String team, String teamAllowed, String style) {
         User user = entityManager.getReference(User.class, userId);
         MatchRequirement matchRequirement = matchRequirementRepository.findUserMatchRequirement(userId);
