e68c359990150b7a5aff7ff55f07df17217c0624
hyeumm <20221159@sungshin.ac.kr>
2026-03-13 15:51:38 +0900

    [feat/#217] 기존 api를 상위 service 로 옮김

--- src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
@@ -1,6 +1,5 @@
 package at.mateball.domain.matchrequirement.core.service;
 
-import at.mateball.domain.matchrequirement.api.dto.response.MatchRequirementV3Res;
 import at.mateball.domain.matchrequirement.core.MatchRequirement;
 import at.mateball.domain.matchrequirement.core.constant.Style;
 import at.mateball.domain.matchrequirement.core.constant.TeamAllowed;
@@ -12,7 +11,6 @@ import at.mateball.exception.BusinessException;
 import at.mateball.exception.code.BusinessErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -42,15 +40,10 @@ public class MatchRequirementV3Service {
         matchRequirementRepository.save(matchRequirement);
     }
 
-    public MatchRequirementV3Res getMatchRequirement(Long userId) {
-        MatchRequirement requirement = Optional.ofNullable(matchRequirementRepository.findUserMatchRequirement(userId))
+    public MatchRequirement getMatchRequirement(Long userId) {
+        MatchRequirement matchRequirement = Optional.ofNullable(matchRequirementRepository.findUserMatchRequirement(userId))
                 .orElseThrow(() -> new BusinessException(MATCH_REQUIREMENT_NOT_FOUND));
 
-        return new MatchRequirementV3Res(
-                TeamName.from(requirement.getTeam()).getLabel(),
-                TeamAllowed.from(requirement.getTeamAllowed()).getLabel(),
-                Style.from(requirement.getStyle()).getLabel(),
-                requirement.getUser().getAvgSeason()
-        );
+        return matchRequirement;
     }
 }
--- src/main/java/at/mateball/domain/matchrequirement/core/service/OnboardingService.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/OnboardingService.java
@@ -1,5 +1,10 @@
 package at.mateball.domain.matchrequirement.core.service;
 
+import at.mateball.domain.matchrequirement.api.dto.response.MatchRequirementV3Res;
+import at.mateball.domain.matchrequirement.core.MatchRequirement;
+import at.mateball.domain.matchrequirement.core.constant.Style;
+import at.mateball.domain.matchrequirement.core.constant.TeamAllowed;
+import at.mateball.domain.team.core.TeamName;
 import at.mateball.domain.user.core.service.UserV3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +20,19 @@ public class OnboardingService {
     public void setOnboardingInformation(Long userId, String team, String teamAllowed, String style, int avgSeason) {
         userV3Service.setAvgSeason(userId, avgSeason);
         matchRequirementV3Service.setMatchRequirement(userId, team, teamAllowed, style);
+    }
+
+    public MatchRequirementV3Res getMatchRequirementAndAvgSeason(Long userId) {
+        int avgSeason = userV3Service.getAvgSeason(userId);
+        MatchRequirement matchRequirement = matchRequirementV3Service.getMatchRequirement(userId);
+
+        return new MatchRequirementV3Res(
+                TeamName.from(matchRequirement.getTeam()).getLabel(),
+                TeamAllowed.from(matchRequirement.getTeamAllowed()).getLabel(),
+                Style.from(matchRequirement.getStyle()).getLabel(),
+                avgSeason
+        );
+
+
     }
 }
