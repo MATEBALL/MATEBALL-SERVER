b9f8fca29414a5bf4ce1b9f1263cec1103972e25
hyeumm <20221159@sungshin.ac.kr>
2026-03-13 13:27:08 +0900

    [chore/#211] userRepository 로 user 조회

--- src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
+++ src/main/java/at/mateball/domain/matchrequirement/core/service/MatchRequirementV3Service.java
@@ -6,30 +6,21 @@ import at.mateball.domain.matchrequirement.core.constant.TeamAllowed;
 import at.mateball.domain.matchrequirement.core.repository.MatchRequirementRepository;
 import at.mateball.domain.team.core.TeamName;
 import at.mateball.domain.user.core.User;
-import at.mateball.domain.user.core.service.UserV3Service;
+import at.mateball.domain.user.core.repository.UserRepository;
 import at.mateball.exception.BusinessException;
 import at.mateball.exception.code.BusinessErrorCode;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 
 public class MatchRequirementV3Service {
     private final MatchRequirementRepository matchRequirementRepository;
-    private final UserV3Service userV3Service;
-    private final EntityManager entityManager;
+    private final UserRepository userRepository;
 
-    @Transactional
-    public void setAvgSeasonAndMatchRequirement(Long userId, String team, String teamAllowed, String style, int avgSeason) {
-        userV3Service.setAvgSeason(userId, avgSeason);
-        setMatchRequirement(userId, team, teamAllowed, style);
-    }
-
-    private void setMatchRequirement(Long userId, String team, String teamAllowed, String style) {
-        User user = entityManager.getReference(User.class, userId);
+    public void setMatchRequirement(Long userId, String team, String teamAllowed, String style) {
+        User user = userRepository.getReferenceById(userId);
         MatchRequirement matchRequirement = matchRequirementRepository.findUserMatchRequirement(userId);
 
         if (matchRequirement != null) {
@@ -42,6 +33,6 @@ public class MatchRequirementV3Service {
                 teamAllowed == null ? TeamAllowed.NO_PREFERENCE.getValue() : TeamAllowed.fromLabel(teamAllowed).getValue(),
                 Style.fromLabel(style).getValue()
         );
-        entityManager.persist(matchRequirement);
+        matchRequirementRepository.save(matchRequirement);
     }
 }
