68f2d9bc264d726492a51ecfcf26821ee757beb6
hyeumm <20221159@sungshin.ac.kr>
2026-03-16 02:34:28 +0900

    [chore/#222] merge 실수 복구

--- src/main/java/at/mateball/domain/matchrequirement/api/controller/MatchRequirementV3Controller.java
+++ src/main/java/at/mateball/domain/matchrequirement/api/controller/MatchRequirementV3Controller.java
@@ -73,5 +73,14 @@ public class MatchRequirementV3Controller {
         matchRequirementAndAvgSeasonService.updateMatchRequirementAndAvgSeason(userId, matchRequirementUpdateReq);
 
         return ResponseEntity.ok(MateballResponse.successWithNoData(SuccessCode.NO_CONTENT));
+      
+    @DeleteMapping
+    public ResponseEntity<MateballResponse<?>> deleteMatchRequirement(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        Long userId = customUserDetails.getUserId();
+        matchRequirementV3Service.deleteByUserId(userId);
+
+        return ResponseEntity.ok(MateballResponse.success(SuccessCode.NO_CONTENT, null));
     }
 }
