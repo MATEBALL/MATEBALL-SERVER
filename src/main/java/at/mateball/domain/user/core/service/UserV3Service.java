b3e5ef19285e3fd0aa086ab7d99c8d0eaa4b9617
hyeumm <20221159@sungshin.ac.kr>
2026-03-16 03:30:34 +0900

    [chore/#229] 충돌 해결하면서 생긴 중복 메소드 삭제

--- src/main/java/at/mateball/domain/user/core/service/UserV3Service.java
+++ src/main/java/at/mateball/domain/user/core/service/UserV3Service.java
@@ -41,12 +41,5 @@ public class UserV3Service {
             findUser(userId).updateAvgSeason(avgSeason);
         }
     }
-      
-    @Transactional
-    public void clearOnboardingInfo(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new BusinessException(BusinessErrorCode.USER_NOT_FOUND));
 
-        user.clearOnboardingInfo();
-    }
 }
