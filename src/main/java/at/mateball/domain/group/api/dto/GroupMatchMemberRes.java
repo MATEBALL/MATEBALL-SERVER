9629ba8203d5b2063ba11f49bf73e2f83d866751
YangDaJin0104 <didekwls0104@naver.com>
2026-03-15 17:34:23 +0900

    [fix/#219] 응답 구조 수정

--- src/main/java/at/mateball/domain/group/api/dto/GroupMatchMemberRes.java
+++ src/main/java/at/mateball/domain/group/api/dto/GroupMatchMemberRes.java
@@ -2,9 +2,15 @@ package at.mateball.domain.group.api.dto;
 
 public record GroupMatchMemberRes(
         Long memberId,
+        Long matchRate,
+        Long age,
+        String gender,
         String nickname,
+        String introduction,
         String team,
         String type,
+        Integer avgGame,
+        Integer avgSeason,
         String img
 ) {
 }
