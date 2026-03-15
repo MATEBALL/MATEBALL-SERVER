fef30ceabaa19935acaa257c1282592d5c6d260a
hyeumm <20221159@sungshin.ac.kr>
2026-03-15 20:08:49 +0900

    [add/#222] MatchRequirementUpdateReq

---
+++ src/main/java/at/mateball/domain/matchrequirement/core/repository/querydsl/MatchRequirementUpdateReq.java
@@ -0,0 +1,16 @@
+package at.mateball.domain.matchrequirement.core.repository.querydsl;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MatchRequirementUpdateReq(
+        @Schema(description = "응원하는 팀 이름")
+        String team,
+        @Schema(description = "응원 팀 허용 여부")
+        String teamAllowed,
+        @Schema(description = "시즌 평균 직관 수")
+        int avgSeason,
+        @Schema(description = "관람스타일")
+        String style
+) {
+}
+
