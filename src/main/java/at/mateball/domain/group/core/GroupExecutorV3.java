53d0f641b892f76164a0da4a3e9b2f211b5635a7
hyeumm <20221159@sungshin.ac.kr>
2026-03-20 22:43:00 +0900

    [chore/#237] 코드리뷰 반영 - unique 예외 처리

--- src/main/java/at/mateball/domain/group/core/GroupExecutorV3.java
+++ src/main/java/at/mateball/domain/group/core/GroupExecutorV3.java
@@ -9,10 +9,11 @@ import at.mateball.domain.user.core.User;
 import at.mateball.exception.BusinessException;
 import at.mateball.exception.code.BusinessErrorCode;
 import jakarta.persistence.EntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import static at.mateball.exception.code.BusinessErrorCode.EXCEED_MATCHING_LIMIT;
 
 @Component
 public class GroupExecutorV3 {
@@ -27,27 +28,33 @@ public class GroupExecutorV3 {
 
     @Transactional
     public Long createGroup(Long userId, Long gameId, boolean isGroup) {
-        User user = entityManager.find(User.class, userId);
-        if (user == null) {
-            throw new BusinessException(BusinessErrorCode.USER_NOT_FOUND);
+
+        try {
+            User user = entityManager.find(User.class, userId);
+
+            if (user == null) {
+                throw new BusinessException(BusinessErrorCode.USER_NOT_FOUND);
+            }
+
+            GameInformation game = entityManager.find(GameInformation.class, gameId);
+
+            Chatting chatting = chattingV2Service.assignChatting();
+            if (chatting == null) {
+                throw new BusinessException(BusinessErrorCode.CHATTING_NOT_FOUND);
+            }
+
+            Group group = Group.create(user, game, isGroup);
+            group.assignChatting(chatting);
+            entityManager.persist(group);
+            entityManager.flush();
+
+            GroupMember leader = GroupMember.leader(user, group, GroupMemberStatus.PENDING_REQUEST.getValue());
+            entityManager.persist(leader);
+
+            return group.getId();
+
+        } catch (DataIntegrityViolationException e) {
+            throw new BusinessException(EXCEED_MATCHING_LIMIT);
         }
-
-        GameInformation game = entityManager.find(GameInformation.class, gameId);
-
-        Chatting chatting = chattingV2Service.assignChatting();
-        if (chatting == null) {
-            throw new BusinessException(BusinessErrorCode.CHATTING_NOT_FOUND);
-        }
-
-        Group group = Group.create(user, game, isGroup);
-        group.assignChatting(chatting);
-
-        entityManager.persist(group);
-        entityManager.flush();
-
-        GroupMember leader = GroupMember.leader(user, group, GroupMemberStatus.PENDING_REQUEST.getValue());
-        entityManager.persist(leader);
-
-        return group.getId();
     }
 }
